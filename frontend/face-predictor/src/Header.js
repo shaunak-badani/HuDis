a100a08f01b11f448eea2b35128ecf44fef27635
Shaunak Badani <shaunak.badani@duke.edu>
2024-11-09 19:22:51 -0500

    Added HuDis logo ; Cleaned up files

--- frontend/face-predictor/src/Header.js
+++ frontend/face-predictor/src/Header.js
@@ -1,11 +1,10 @@
 import React from "react";
-import { Typography } from "@mui/material";
+import HuDis from "./images/HuDis_Full.png";
 
 
 const Header = (props) => (
     <div className="max-w-4xl sm:py-7 bg-white min-w-full min-h-20 drop-shadow-xl flex items-center justify-center">
-        {/* <img className="h-14 m-10" src={flashback} /> */}
-        <Typography variant="h2">HuDis?</Typography>
+        <img className="h-14" src={HuDis} alt="HuDis logo"/>
     </div>
 );
 
